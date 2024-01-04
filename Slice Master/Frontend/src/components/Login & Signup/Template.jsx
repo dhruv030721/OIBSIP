21947a06e92598b9295275a8ba6a5a53d8b09855
Mr.Programmer <dhruvgodhani07@gmail.com>
2024-01-05 00:15:22 +0530

    Design login page and some background and also animated logo

--- Slice Master/Frontend/src/components/Login & Signup/Template.jsx
+++ Slice Master/Frontend/src/components/Login & Signup/Template.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import SideBackground from '../../assets/wallpaper/wallpaper-1.jpeg'
+import Background from '../../assets/wallpaper/background.png'
+import PizzaIcon from '../../assets/wallpaper/pizzaIcon.png'
 
-function Template({formType}) {
+function Template({ formType }) {
   return (
     <div className='flex h-screen justify-between'>
-      <div className='flex flex-col items-center w-[40%]'>
-        <div className="logo-text font-parisienne font-black text-6xl">Slice Master</div>
-        <h1 className="welcome-text font-poppins font-extrabold text-2xl">Welcome To Back</h1>
-        <p className="description"></p>
-        {formType}
+      <div className='flex flex-col items-center w-[40%] before:bg-pizza-background before:absolute before:w-[40%] before:h-[100%] before:bg-no-repeat before:-z-10 space-y-7 before:opacity-15 '>
+        <div className='flex py-5'>
+          <h1 className='logo-text font-parisienne font-black text-6xl first-letter:text-orange-500 '>Slice&#160;</h1>
+          <h1 className='logo-text font-parisienne font-black text-6xl first-letter:text-red-500 '>Master</h1>
+          <img src={PizzaIcon} alt="" className='w-20 h-20 drop-shadow-2xl animate-wiggle' />
+        </div>
+          <h1 className="welcome-text font-poppins font-extrabold text-3xl">Welcome To Back</h1>
+          {formType}
       </div>
       <div className='w-[60%] h-screen'>
         {/* Photo  */}
-        <img src={SideBackground} alt="" className='object-cover w-[100%] h-[100%]'/>
+        <img src={SideBackground} alt="" className='object-cover w-[100%] h-[100%]' />
       </div>
     </div>
   )
--- Slice Master/Frontend/tailwind.config.js
+++ Slice Master/Frontend/tailwind.config.js
@@ -6,9 +6,21 @@ export default {
   ],
   theme: {
     extend: {
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'translatey(-10px)' },
+          '50%': { transform: 'translatey(0px)' },
+        }
+      },
       fontFamily : {
         'parisienne' : ['Parisienne'],
         'poppins' : ['Poppins'],
+      },
+      animation: {
+        wiggle: 'wiggle 1.5s ease-in-out infinite',
+      },
+      backgroundImage: {
+        'pizza-background': "url('./assets/wallpaper/background.png')"
       }
     },
   },
