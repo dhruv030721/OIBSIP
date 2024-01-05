7fa04ab7014189ef6b6b3c1976bf6fa58d3d4dca
Mr.Programmer <dhruvgodhani07@gmail.com>
2024-01-05 23:31:26 +0530

    Design signup page

--- Slice Master/Frontend/src/components/Login & Signup/Template.jsx
+++ Slice Master/Frontend/src/components/Login & Signup/Template.jsx
@@ -12,7 +12,7 @@ function Template({ formType }) {
           <h1 className='logo-text font-parisienne font-black text-6xl first-letter:text-red-500 '>Master</h1>
           <img src={PizzaIcon} alt="" className='w-20 h-20 animate-wiggle drop-shadow-3xl' />
         </div>
-          <h1 className="welcome-text font-poppins font-extrabold text-3xl">Welcome To Back</h1>
+          <h1 className="text-center font-poppins font-extrabold text-3xl">Welcome To Back</h1>
           {formType}
       </div>
       <div className='w-[60%] h-screen'>
--- Slice Master/Frontend/src/main.jsx
+++ Slice Master/Frontend/src/main.jsx
@@ -13,6 +13,7 @@ const router = createBrowserRouter(
           <Route index element={<Home/>} />
         </Route>
         <Route path='login' element={<Login />}/>
+        <Route path='signup' element={<Signup />}/>
     </Route>
   )
 
