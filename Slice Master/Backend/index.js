4ab51f134cb59c2fb8422e919b1dcc4a74e31c30
Mr.Programmer <dhruvgodhani07@gmail.com>
2024-06-24 21:48:44 +0530

    Allow all origin and include morgan package

--- Slice Master/Backend/index.js
+++ Slice Master/Backend/index.js
@@ -5,17 +5,21 @@ const app = express();
 const server = http.createServer(app)
 const cors = require("cors")
 initializeSocket(server)
+const morgan = require('morgan');
+
 
 require('dotenv').config();
 const PORT = process.env.PORT || 4000
 
 const corsOptions = {
-    origin: 'https://slice-master-flax.vercel.app/',
+    origin: '*',
     optionsSuccessStatus: 200,  
   };
 
 app.use(cors(corsOptions))
 app.use(express.json());    
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+
 
 const fileUpload = require("express-fileupload");
 app.use(fileUpload({
